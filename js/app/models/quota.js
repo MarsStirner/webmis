73a020926bcf14c79d9f5b3fe46f0b8d7b82dc3e
Fedor Kurilov <fkuril@mail.ru>
2012-12-14 14:12:58 +0400

    [feature] Модель квоты

--- js/app/models/quota.js
+++ js/app/models/quota.js
@@ -2,11 +2,54 @@
  * User: FKurilov
  * Date: 13.12.12
  */
-define(function () {
+define(["models/mkb", "models/department"], function () {
+	var QuotaType = Model.extend({
+		defaults: {
+			"name": ""
+		}
+	});
+
+	var QuotaStatus = Model.extend({
+		defaults: {
+			"name": ""
+		}
+	});
+
 	App.Models.Quota = Model.extend({
 		defaults: {
+			"version": "",
+			"appealNumber": "",
+			"talonNumber": "",
+			"stage": "",
+			"request": "",
+			"mkb": {},
+			"quotaType": {},
+			"department": {},
+			"status": {}
+		},
 
-		}
+		relations: [
+			{
+				type: Backbone.HasOne,
+				key: "mkb",
+				relatedModel: App.Models.Mkb
+			},
+			{
+				type: Backbone.HasOne,
+				key: "quotaType",
+				relatedModel: QuotaType
+			},
+			{
+				type: Backbone.HasOne,
+				key: "department",
+				relatedModel: App.Models.Department
+			},
+			{
+				type: Backbone.HasOne,
+				key: "status",
+				relatedModel: QuotaStatus
+			}
+		]
 	});
 
 	return App.Models.Quota;
