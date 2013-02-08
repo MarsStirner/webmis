bd4fa62c40e7613014c435f6d2d276f7a2545f09
Fedor Kurilov <fkuril@mail.ru>
2013-02-08 16:03:24 +0400

    Очистка от тегов

--- js/app/models/print/appeal.js
+++ js/app/models/print/appeal.js
@@ -3,10 +3,20 @@ define([
 ], function () {
 
 	App.Models.PrintAppeal = App.Models.Appeal.extend({
-		urlRoot: function(){
+		urlRoot: function () {
 			return DATA_PATH + "print/appeals/"
+		},
+
+		toJSON: function () {
+			var json = App.Models.Appeal.prototype.apply(this);
+			if (json.diagnoses.length) {
+				_.each(json.diagnoses, function (d) {
+					var tags = $(d.description);
+					if (tags.length) d.description = tags.map(function (t) { return $(t).text(); }).join(" ");
+				});
+			}
 		}
 	});
 
-	return App.Models.Appeal;
+	return App.Models.PrintAppeal;
 } );
