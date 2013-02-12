77ff7cea87248cd0d3aedd22ea8d7ab87196fef3
Alexandr Voronkin <admin@elasticweb.ru>
2013-02-12 15:54:25 +0400

    фиксы

--- data/biomaterials.php
+++ data/biomaterials.php
@@ -8,8 +8,8 @@ if ( !empty($_GET["callback"]) ) {
 }
 
 echo $callback_name ?>({"requestData":{"filter":{"fullName":""}},"data":[
-{id: '1',fullName: 'Фамилия И.О.',sex: 'М',birthDate: '829512000000',actionType: {name: 'Биохимия крови "МИКРО"',isUrgent:true},testTube: {name:'MONOVETTE 4',color:'#3399cc'},status: {name:'ожидается',code:0}},
-{id: '2',fullName: 'Фамилия И.О.',sex: 'М',birthDate: '829512000000',actionType: {name: 'Биохимия крови "МИКРО"',isUrgent:false},testTube: {name:'MONOVETTE 2',color:'#9999cc'},status: {name:'ожидается',code:0}},
-{id: '3',fullName: 'Фамилия И.О.',sex: 'М',birthDate: '829512000000',actionType: {name:'Биохимия крови "МИКРО"',isUrgent:false},testTube: {name:'MONOVETTE 2',color:'#9999cc'},status: {name:'ожидается',code:0}},
-{id: '4',fullName: 'Фамилия И.О.',sex: 'М',birthDate: '829512000000',actionType: {name: 'Биохимия крови "МИКРО"',isUrgent:true},testTube: {name:'MONOVETTE 2',color:'#9999cc'},status: {name:'выполняется',code:1}},
-{id: '5',fullName: 'Фамилия И.О.',sex: 'М',birthDate: '829512000000',actionType: {name:'Биохимия крови "МИКРО"',isUrgent:false},testTube: {name:'MONOVETTE 2',color:'#9999cc'},status: {name:'закончено',code:2}}]});
+{id: '1',datetime:1356072424000,fullName: 'Фамилия И.О.',sex: 'М',birthDate: '829512000000',actionType: {name: 'Биохимия крови "МИКРО"',isUrgent:true},testTube: {name:'MONOVETTE 4',color:'#3399cc'},status: {name:'ожидается',code:0}},
+{id: '2',datetime:1356072424000,fullName: 'Фамилия И.О.',sex: 'М',birthDate: '829512000000',actionType: {name: 'Биохимия крови "МИКРО"',isUrgent:false},testTube: {name:'MONOVETTE 2',color:'#9999cc'},status: {name:'ожидается',code:0}},
+{id: '3',datetime:1356072424000,fullName: 'Фамилия И.О.',sex: 'М',birthDate: '829512000000',actionType: {name:'Биохимия крови "МИКРО"',isUrgent:false},testTube: {name:'MONOVETTE 2',color:'#9999cc'},status: {name:'ожидается',code:0}},
+{id: '4',datetime:1356072424000,fullName: 'Фамилия И.О.',sex: 'М',birthDate: '829512000000',actionType: {name: 'Биохимия крови "МИКРО"',isUrgent:true},testTube: {name:'MONOVETTE 2',color:'#9999cc'},status: {name:'выполняется',code:1}},
+{id: '5',datetime:1356072424000,fullName: 'Фамилия И.О.',sex: 'М',birthDate: '829512000000',actionType: {name:'Биохимия крови "МИКРО"',isUrgent:false},testTube: {name:'MONOVETTE 2',color:'#9999cc'},status: {name:'закончено',code:2}}]});
