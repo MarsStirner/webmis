e2717e7cfc5e071ca755bb20645c8cb76df52eb6
Alexandr Voronkin <admin@elasticweb.ru>
2013-02-20 10:59:01 +0400

    Фильтр по типу исследований

--- js/app/views/pages/BiomaterialsView.js
+++ js/app/views/pages/BiomaterialsView.js
@@ -1,7 +1,8 @@
 define(['text!templates/pages/biomaterials.tmpl',
 	'collections/Biomaterials',
 	'views/grid',
-	'views/pages/BiomaterialsCountsView'], function (biomaterialsTemplate, BiomaterialsCollection, GridView, CountView) {
+	'views/pages/BiomaterialsCountsView',
+  'collections/dictionary-values'], function (biomaterialsTemplate, BiomaterialsCollection, GridView, CountView) {
 
 	var BiomaterialsView = View.extend({
 		///className: "ContentHolder",
@@ -10,6 +11,9 @@ define(['text!templates/pages/biomaterials.tmpl',
 
 			var biomaterialsCollection = new BiomaterialsCollection;
 			this.collection = biomaterialsCollection;
+
+
+
 
 			this.grid = new GridView({
 				collection: this.collection,
@@ -25,16 +29,41 @@ define(['text!templates/pages/biomaterials.tmpl',
 
 			this.collection.fetch();
 
+			// Получаем справочник типов биоматериалов
+			this.tissues = new App.Collections.DictionaryValues("", {
+				name: "tissueTypes"
+			});
+			this.tissues.on("reset", this.onTissuesLoaded, this);
+			this.tissues.fetch();
+
 
 		},
+
+		onTissuesLoaded: function (coll) {
+			console.log('onTissuesLoaded',this.tissues)
+			_(this.tissues.toJSON()).each(function (item) {
+				this.$(".tissues").append($("<option/>", {
+					"text": item.value,
+					"value": item.id
+				}));
+			}, this);
+
+			this.$(".tissues").select2("enable");
+
+		},
+
 		render: function () {
 			var view = this;
 
 			view.$el.html($.tmpl(view.template));
+
 			view.$("#biomaterial-grid").html(view.grid.el);
 			view.$("#biomaterial-count table").append(view.counts.el);
 
-			//view.counts.render();
+			if (!this.$(".tissues").find("option").length) {
+				this.$(".tissues").select2("disable");
+			}
+
 
 			UIInitialize(view.el);
 //			this.delegateEvents();
