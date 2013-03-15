a5365fbdcb49addda148d5bf558e91473ecf24e1
Alexandr Voronkin <admin@elasticweb.ru>
2013-03-15 10:09:58 +0400

    fix WEBMIS-694

--- js/app/collections/department-patients.js
+++ js/app/collections/department-patients.js
@@ -13,6 +13,31 @@ define(["models/department-patient"], function () {
 		},
 		url: function (){
 			return DATA_PATH + "departments/patients/";
+		},
+		parse: function (raw){
+			checkForWarnings(raw.requestData, "requestData was not found in the JSON");
+			this.requestData = raw.requestData || {};
+			this.requestData.filter = this.requestData.filter || {};
+
+			if(raw.data && raw.data.length > 0){
+				_.each(raw.data, function (patient, index, list){
+					//нужно выводить целые числа
+					if(patient.condition && patient.condition.breathingRate){
+						patient.condition.breathingRate = parseInt(patient.condition.breathingRate);
+					}
+
+					if(patient.condition && patient.condition.arterialBloodPressure && patient.condition.arterialBloodPressure.low){
+						patient.condition.arterialBloodPressure.low = parseInt(patient.condition.arterialBloodPressure.low);
+					}
+
+					if(patient.condition && patient.condition.arterialBloodPressure && patient.condition.arterialBloodPressure.high){
+						patient.condition.arterialBloodPressure.high = parseInt(patient.condition.arterialBloodPressure.high);
+					}
+
+				})
+			}
+
+			return raw.data
 		}
 	});
 });
