04f8a06a802186daaabac66c11ccc26d3a13422a
Alexandr Voronkin <admin@elasticweb.ru>
2013-05-14 17:35:42 +0400

    fix WEBMIS-779

--- js/app/models/department-patient.js
+++ js/app/models/department-patient.js
@@ -2,53 +2,44 @@
  * User: FKurilov
  * Date: 25.05.12
  */
-define(["models/Name", "models/Bed", "models/doctor", "models/condition"], function (Name) {
+define(["models/Name", "models/Bed", "models/doctor", "models/condition"], function(Name) {
 	App.Models.DepartmentPatient = Model.extend({
 		defaults: {
 			number: "",
 			name: {},
 			birthDate: "",
 			hospitalBed: {},
+			movingTo: null,
 			doctor: {},
 			condition: {},
 			createDateTime: "",
 			checkOut: ""
 		},
 
-		relations: [
-			{
-				type: Backbone.HasOne,
-				key: "name",
-				relatedModel: Name
-			},
-			{
-				type: Backbone.HasOne,
-				key: "hospitalBed",
-				//relatedModel: "App.Models.HospitalBed",
-				relatedModel: "App.Models.Bed"
-			},
-			{
-				type: Backbone.HasOne,
-				key: "doctor",
-				relatedModel: "App.Models.Doctor"
-			},
-			{
-				type: Backbone.HasOne,
-				key: "condition",
-				relatedModel: "App.Models.Condition"
-			}
-		],
+		relations: [{
+			type: Backbone.HasOne,
+			key: "name",
+			relatedModel: Name
+		}, {
+			type: Backbone.HasOne,
+			key: "hospitalBed",
+			//relatedModel: "App.Models.HospitalBed",
+			relatedModel: "App.Models.Bed"
+		}, {
+			type: Backbone.HasOne,
+			key: "doctor",
+			relatedModel: "App.Models.Doctor"
+		}, {
+			type: Backbone.HasOne,
+			key: "condition",
+			relatedModel: "App.Models.Condition"
+		}],
 
-		parse: function ( raw ) {
+		parse: function(raw) {
 			var depPatient = new App.Models.DepartmentPatient;
 			var data = raw.data ? raw.data : raw;
-//			console.log('data',data);
-//
-//			console.log('depPatient',depPatient);
-//			console.log('depPatient.toJSON',depPatient.toJSON());
-			var val = Core.Objects.mergeAll( depPatient.toJSON(), data );
 
-//			console.log('return data',val);
+			var val = Core.Objects.mergeAll(depPatient.toJSON(), data);
 
 			return val;
 		}
