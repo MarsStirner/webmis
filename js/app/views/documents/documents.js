141ad32d62d2974847fbcce288b8340fdd836dab
Fedor Kurilov <fkuril@mail.ru>
2013-05-21 10:41:07 +0400

    проектирование структуры модуля

--- js/app/views/documents/documents.js
+++ js/app/views/documents/documents.js
@@ -3,10 +3,8 @@
  * Date: 20.05.13
  */
 define(function (require) {
-	/**
-	 * Структура модуля
-	 * @type {{Views: {}, Collections: {}, Models: {}}}
-	 */
+
+	//Структура модуля
 	var Documents = {
 		Views: {
 			List: {},
@@ -44,29 +42,47 @@ define(function (require) {
 
 	//Редактирование
 	Documents.Views.Edit.Layout = Backbone.View.extend({});
+	//Верхний блок элементов управления и навигации
 	Documents.Views.Edit.NavControls = Backbone.View.extend({});
+	//Управление сохранением документа
 	Documents.Views.Edit.DocControls = Backbone.View.extend({});
+	//Сетка (12 колонок по умолчанию)
 	Documents.Views.Edit.Grid = Backbone.View.extend({});
+	//Ряд в сетке
 	Documents.Views.Edit.GridRow = Backbone.View.extend({});
+	//Ячейка в сетке
 	Documents.Views.Edit.GridRowSpan = Backbone.View.extend({});
 
+	//Базовый класс UI элемента для поля документа
 	Documents.Views.Edit.UIElement.Base = Backbone.View.extend({});
 
 	//Shortcut
 	var UIElementBase = Documents.Views.Edit.UIElement.Base;
 
+	//Поле типа Constructor
 	Documents.Views.Edit.UIElement.Constructor = UIElementBase.extend({});
+	//Поле типа String
 	Documents.Views.Edit.UIElement.String = UIElementBase.extend({});
+	//Поле типа Text
 	Documents.Views.Edit.UIElement.Text = UIElementBase.extend({});
+	//Поле типа Time
 	Documents.Views.Edit.UIElement.Time = UIElementBase.extend({});
+	//Поле типа Date
 	Documents.Views.Edit.UIElement.Date = UIElementBase.extend({});
+	//Поле типа Integer
 	Documents.Views.Edit.UIElement.Integer = UIElementBase.extend({});
+	//Поле типа Double
 	Documents.Views.Edit.UIElement.Double = UIElementBase.extend({});
+	//Поле типа MKB
 	Documents.Views.Edit.UIElement.MKB = UIElementBase.extend({});
+	//Поле типа FlatDirectory
+	Documents.Views.Edit.UIElement.FlatDirectory = UIElementBase.extend({});
 
 	//Просмотр
 	Documents.Views.Review.Layout = Backbone.View.extend({});
+	//Элементы управления
 	Documents.Views.Review.Controls = Backbone.View.extend({});
+	//Значения полей из документа
 	Documents.Views.Review.DocValues = Backbone.View.extend({});
 
 	return Documents;
