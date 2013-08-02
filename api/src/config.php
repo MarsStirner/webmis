8f1415686bb19ea43cc2c9cf5aeddc7a0a3b9207
Alexandr Voronkin <admin@elasticweb.ru>
2013-08-02 19:38:23 +0400

    fix

--- api/src/config.php
+++ api/src/config.php
@@ -1,32 +1,6 @@
 <?php
 
-use Webmis\Services\JsonpResponce;
-
-
-
-$app['debug'] = true;
-$app["log.level"] = 100;
-
-require __DIR__.'/db-config.php';
-$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
-    'db.options' => array(
-        'driver'   => 'pdo_mysql',
-        'host'=> $db_host,
-        'dbname'=> $db_name,
-        'user'=> $db_user,
-        'password'=> $db_password,
-        'charset'=>'utf8'
-    ),
-));
-
-$app['jsonp'] = function ($app) {
-    return new JsonpResponce($app);
-};
-
-
-$app['propel.path'] = __DIR__.'../vendor/propel/propel1/runtime/lib/Propel.php';
-$app['propel.config_file'] = __DIR__.'/propel-conf.php';
-$app['propel.model_path'] = __DIR__.'/Webmis/Models';
-$app->register(new Propel\Silex\PropelServiceProvider());
-
-
+$db_host = '10.1.2.106';//хост базы данных
+$db_name = 's11r64';//имя базы данных
+$db_user = 'root';//пользователь базы данных
+$db_password = 'root';//пароль пользователя базы данных
