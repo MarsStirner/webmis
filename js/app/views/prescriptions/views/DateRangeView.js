291f96131c60de271475076f92cca20ce981ffd2
Alexandr Voronkin <admin@elasticweb.ru>
2013-12-10 16:04:42 +0400

    сетка часов

--- js/app/views/prescriptions/views/DateRangeView.js
+++ js/app/views/prescriptions/views/DateRangeView.js
@@ -1,48 +1,64 @@
-define(function(require) {
-	var template = require('text!../templates/date-range.html');
-	var BaseView = require('./BaseView');
-	var rivets = require('rivets');
+define(function (require) {
+    var template = require('text!../templates/date-range.html');
+    var BaseView = require('./BaseView');
+    var rivets = require('rivets');
 
-	return BaseView.extend({
-		template: template,
-		initialize: function() {
+    return BaseView.extend({
+        template: template,
+        initialize: function () {
 
-			if (!this.model.get('dateRangeMin')) {
-				this.model.set('dateRangeMin', moment().startOf('day').format('X'));
-				this.model.set('dateRangeMax', moment().startOf('day').add('days', 1).format('X'));
-			}
-		},
-		data: function(){
-			console.log('dateRange data',this.model.toJSON());
-			return this.model.toJSON();
-		},
-		afterRender: function() {
-			this.ui = {};
-			this.ui.$dateRangeMin = this.$el.find("#dateRangeMin");
-			this.ui.$dateRangeMin.datepicker();
+            if (!this.model.get('dateRangeMin')) {
+                this.model.set('dateRangeMin', moment()
+                    .startOf('day')
+                    .add('hours',12)
+                    .format('X'));
 
-			this.ui.$dateRangeMax = this.$el.find("#dateRangeMax");
-			this.ui.$dateRangeMax.datepicker();
+                this.model.set('dateRangeMax', moment().startOf('day').add('days', 1).add('hours', 12).format('X'));
+            }
+            this.listenTo(this.model, 'change:dateRangeMin', this.setDataRangeMax);
+        },
+        setDataRangeMax: function (model, dateRangeMin) {
+            var dateRangeMax = moment(dateRangeMin * 1000)
+                .add('days', 1)
+                .format('X');
+            this.model.set('dateRangeMax', dateRangeMax);
+            console.log('setDataRangeMax', arguments);
+        },
+        data: function () {
+            console.log('dateRange data', this.model.toJSON());
+            return this.model.toJSON();
+        },
+        afterRender: function () {
+            this.ui = {};
+            this.ui.$dateRangeMin = this.$el.find("#dateRangeMin");
+            this.ui.$dateRangeMin.datepicker();
 
-			// this.ui.$dateRangeType = this.$el.find("#dateRangeType");
-			// this.ui.$dateRangeType.buttonset();
+            rivets.formatters.date = {
+                read: function (value) {
+                    var output = moment(value, 'X')
+                        .format('DD.MM.YYYY');
+                    return output;
+                },
+                publish: function (value) {
+                    var output = moment(value, 'DD.MM.YYYY').add('hours', 12).format('X');
+                    return output;
+                }
+            };
+            rivets.formatters.dateTime = function (value) {
+                return moment(value,'X')
+                    .format('DD.MM.YYYY HH:mm');
+            };
+            rivets.formatters.time = function (value) {
+                return moment(value,'X')
+                    .format('HH:mm');
+            };
 
-			rivets.formatters.date = {
-				read: function(value) {
-					var output = moment(value, 'X').format('DD.MM.YYYY');
-					return output;
-				},
-				publish: function(value) {
-					var output = moment(value, 'DD.MM.YYYY').format('X');
-					return output;
-				}
-			}
 
-			rivets.bind(this.el, {
-				filter: this.model
-			});
-		}
+            rivets.bind(this.el, {
+                filter: this.model
+            });
+        }
 
-	});
+    });
 
 });
