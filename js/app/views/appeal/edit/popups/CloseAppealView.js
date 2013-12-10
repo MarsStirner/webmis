b146a3e96a728f611a6f598768c7780db53e0e48
Fedor Kurilov <fkuril@mail.ru>
2013-12-10 19:16:02 +0400

    check last move closed on appeal close

--- js/app/views/appeal/edit/popups/CloseAppealView.js
+++ js/app/views/appeal/edit/popups/CloseAppealView.js
@@ -1,273 +1,262 @@
-define(function(require) {
-    var popupMixin = require('mixins/PopupMixin');
-    var Docs4closing = require('models/Docs4closing');
-    var Results = require('collections/Results');
+define(function (require) {
+	var popupMixin = require('mixins/PopupMixin');
+	var Docs4closing = require('models/Docs4closing');
+	var Results = require('collections/Results');
+	var Moves = require('collections/moves/moves');
 
-    var template = require('text!templates/appeal/edit/popups/close-appeal-popup.html');
+	var template = require('text!templates/appeal/edit/popups/close-appeal-popup.html');
 
-    return View.extend({
-        initialize: function() {
-            //console.log('initialize close appeal popup', this);
+	return View.extend({
+		initialize: function () {
+			//console.log('initialize close appeal popup', this);
 
-        },
-        //template:'',
-        events: {
-            'click .save': 'onSave',
-            'change #results': 'validate' //,
-            //'change .appeal-close-date': 'validate',
-            //'change .appeal-close-time': 'validate'
-        },
-        validate: function() {
-            var result = this.getResult();
-            var closeDate = this.getCloseDate();
-            var errors = [];
+		},
+		//template:'',
+		events: {
+			'click .save': 'onSave',
+			'change #results': 'validate' //,
+			//'change .appeal-close-date': 'validate',
+			//'change .appeal-close-time': 'validate'
+		},
+		validate: function () {
+			var result = this.getResult();
+			var closeDate = this.getCloseDate();
+			var errors = [];
 
-            if (result.text === '...') {
-                errors.push({
-                    message: 'Не выбран результат госпитализации. '
-                });
-            }
+			if (result.text === '...') {
+				errors.push({
+					message: 'Не выбран результат госпитализации. '
+				});
+			}
 
-            //console.log(moment(closeDate).diff(moment()))
+			//console.log(moment(closeDate).diff(moment()))
 
-            if (errors.length === 0) {
-                this.ui.$saveButton.button('enable');
-            } else {
-                this.ui.$saveButton.button('disable');
-            }
-        },
-        getCloseDate: function() {
-            var hour = this.ui.$appealCloseTime.timepicker('getHour');
-            var minute = this.ui.$appealCloseTime.timepicker('getMinute');
-            var date = this.ui.$appealCloseDate.datepicker('getDate');
-            var closeDate = moment(date).minutes(minute).hours(hour).valueOf();
-            return closeDate;
-        },
-        getResult: function() {
-            var resultId = this.ui.$results.val();
-            var resultText = this.$('option:selected', this.ui.$results).text();
+			if (errors.length === 0) {
+				this.ui.$saveButton.button('enable');
+			} else {
+				this.ui.$saveButton.button('disable');
+			}
+		},
 
-            return {
-                id: resultId,
-                text: resultText
-            };
-        },
-        onSave: function() {
-            var self = this;
-            // console.log('onSave');
-            this.ui.$saveButton.button('disable');
+		getCloseDate: function () {
+			var hour = this.ui.$appealCloseTime.timepicker('getHour');
+			var minute = this.ui.$appealCloseTime.timepicker('getMinute');
+			var date = this.ui.$appealCloseDate.datepicker('getDate');
+			var closeDate = moment(date).minutes(minute).hours(hour).valueOf();
+			return closeDate;
+		},
 
-            var closeDate = this.getCloseDate();
+		getResult: function () {
+			var resultId = this.ui.$results.val();
+			var resultText = this.$('option:selected', this.ui.$results).text();
 
-            this.appeal = this.options.appeal;
-            var appealId = this.options.appeal.get('id');
+			return {
+				id: resultId,
+				text: resultText
+			};
+		},
 
-            var closeDocs = this.ui.$closeDocs.prop('checked');
+		onSave: function () {
+			var self = this;
+			// console.log('onSave');
+			this.ui.$saveButton.button('disable');
 
-            var result = this.getResult();
+			var closeDate = this.getCloseDate();
 
-            var when;
-            if (closeDocs) {
-                when = $.when(closeAppealDocs(), vacateBed(), closeAppeal());
-            } else {
-                when = $.when(vacateBed(), closeAppeal());
-            }
+			this.appeal = this.options.appeal;
+			var appealId = this.options.appeal.get('id');
 
+			var closeDocs = this.ui.$closeDocs.prop('checked');
 
+			var result = this.getResult();
 
-            when.then(function() {
-                pubsub.trigger('appeal:closed', {
-                    closed: true,
-                    closeDate: closeDate,
-                    resultText: result.text
-                });
-                self.close();
+			var when;
+			if (closeDocs) {
+				when = $.when(closeAppealDocs(), vacateBed(), closeAppeal());
+			} else {
+				when = $.when(vacateBed(), closeAppeal());
+			}
 
-            }, function() {
-                console.log('ошибка', arguments);
-            })
+			when.then(function () {
+				pubsub.trigger('appeal:closed', {
+					closed: true,
+					closeDate: closeDate,
+					resultText: result.text
+				});
+				self.close();
+			}, function () {
+				console.log('ошибка', arguments);
+			});
 
-            function closeAppealDocs() {
-                //закрываем документы
-                return $.ajax({
-                    url: '/api/v1/appeals/' + appealId + '/docs/close?execDate=' + closeDate,
-                    dataType: 'jsonp',
-                    type: 'GET',
-                    success: function() {
-                        pubsub.trigger('noty', {
-                            text: 'Закрыли документы',
-                            type: 'success'
-                        });
-                        //console.log('success close docs', arguments);
-                    }
-                });
-            };
+			function closeAppealDocs () {
+				//закрываем документы
+				return $.ajax({
+					url: '/api/v1/appeals/' + appealId + '/docs/close?execDate=' + closeDate,
+					dataType: 'jsonp',
+					type: 'GET',
+					success: function () {
+						pubsub.trigger('noty', {
+							text: 'Закрыли документы',
+							type: 'success'
+						});
+						//console.log('success close docs', arguments);
+					}
+				});
+			}
 
-            function vacateBed() {
-                //var closeBedDate = closeDate - (60 * 60 * 1000); //какой-то хак от Марины Владимировны, для поддержки НТК
-                var closeBedDate = closeDate - (1000); //какой-то хак , для поддержки НТК
-                //выписка с койки
-                return $.ajax({
-                    url: '/api/v1/appeals/' + appealId + '/bed/vacate?execDate=' + closeBedDate,
-                    dataType: 'jsonp',
-                    type: 'GET',
-                    success: function(data) {
-                        if (data) {
-                            pubsub.trigger('noty', {
-                                text: 'Выписали с койки',
-                                type: 'success'
-                            });
-                        }
+			function vacateBed () {
+				//var closeBedDate = closeDate - (60 * 60 * 1000); //какой-то хак от Марины Владимировны, для поддержки НТК
+				var closeBedDate = closeDate - (1000); //какой-то хак , для поддержки НТК
+				//выписка с койки
+				return $.ajax({
+					url: '/api/v1/appeals/' + appealId + '/bed/vacate?execDate=' + closeBedDate,
+					dataType: 'jsonp',
+					type: 'GET',
+					success: function (data) {
+						if (data) {
+							pubsub.trigger('noty', {
+								text: 'Выписали с койки',
+								type: 'success'
+							});
+						}
+					}
+				});
+			}
 
-                        //console.log('success close bed', arguments);
-                    }
-                });
-            }
+			function closeAppeal () {
+				//выписка с истории болезни
+				return $.ajax({
+					url: '/api/v1/appeals/' + appealId + '/close',
+					data: {
+						execDate: closeDate,
+						resultId: result.id
+					},
+					dataType: 'jsonp',
+					type: 'GET',
+					success: function () {
+						pubsub.trigger('noty', {
+							text: 'Закрыли историю болезни',
+							type: 'success'
+						});
+						//console.log('success close appeal', arguments);
+					}
+				});
+			}
+		},
 
+		data: function () {
+			var data = {};
 
-            function closeAppeal() {
-                //выписка с истории болезни
-                return $.ajax({
-                    url: '/api/v1/appeals/' + appealId + '/close',
-                    data: {
-                        execDate: closeDate,
-                        resultId: result.id
-                    },
-                    dataType: 'jsonp',
-                    type: 'GET',
-                    success: function() {
-                        pubsub.trigger('noty', {
-                            text: 'Закрыли историю болезни',
-                            type: 'success'
-                        });
-                        //console.log('success close appeal', arguments);
-                    }
-                });
-            }
+			data.doctorId = this.options.appeal.get('execPerson').id;
+			data.docs = this.docs4closing.toJSON();
+			data.appealId = this.options.appeal.get('id');
+			data.lastMoveClosed = this.moves.length && this.moves.last().get("admission") && this.moves.last().get("leave");
 
+			return data;
+		},
 
-        },
+		docs4closing: function () {
 
-        data: function() {
-            var data = {};
+		},
 
-            data.doctorId = this.options.appeal.get('execPerson').id;
+		render: function () {
+			var self = this;
 
-            data.docs = this.docs4closing.toJSON();
-            data.appealId = this.options.appeal.get('id');
+			this.ui = {};
+			this.ui.$saveButton = this.$el.closest(".ui-dialog").find('.save');
+			this.ui.$saveButton.button('disable');
 
-            return data;
-        },
+			this.docs4closing = new Docs4closing();
+			this.docs4closing.appealId = this.options.appeal.get('id');
 
-        render: function() {
-            var self = this;
+			this.docs4closing.on('change', function () {
+				//Движения
+				self.moves = new Moves();
+				self.moves = this.options.appealId;
 
-            this.ui = {};
+				self.moves.on("reset", function () {
+					self.moves.off();
 
-            this.ui.$saveButton = this.$el.closest(".ui-dialog").find('.save');
+					this.$el.html(_.template(template, this.data(), {
+						variable: 'data'
+					}));
 
-            this.ui.$saveButton.button('disable');
+					this.ui.$appealCloseDate = this.$('.appeal-close-date');
+					this.ui.$appealCloseTime = this.$('.appeal-close-time');
+					this.ui.$closeDocs = this.$('.close-docs');
 
-            this.docs4closing = new Docs4closing();
-            this.docs4closing.appealId = this.options.appeal.get('id');
+					var datetime = new Date();
 
-            this.docs4closing.on('change', function() {
+					this.ui.$appealCloseDate.datepicker({
+						minDate: 0,
+						onSelect: function (dateText, inst) {
+							var day = moment(self.$(this).datepicker('getDate')).startOf('day');
+							var currentDay = moment().startOf('day');
+							var currentHour = moment().hour();
+							var hour = self.ui.$appealCloseTime.timepicker('getHour');
+							//если выбрана текущая дата и время в таймпикере меньше текущего, то сбрасываем таймпикер
+							if (day.diff(currentDay, 'days') === 0) {
+								if (hour <= currentHour) {
+									self.ui.$appealCloseTime.timepicker('setTime', (new Date())).trigger('change');
+								}
+							}
+						}
+					}).datepicker('setDate', datetime);
 
-                //console.log('docs4closing', this.docs4closing.toJSON());
-                this.$el.html(_.template(template, this.data(), {
-                    variable: 'data'
-                }));
+					this.ui.$appealCloseTime.timepicker({
+						showPeriodLabels: false,
+						onHourShow: function (hour) {
+							var day = moment(self.ui.$appealCloseDate.datepicker('getDate')).startOf('day');
+							var currentDay = moment().startOf('day');
+							var currentHour = moment().hour();
+							//если выбран текущий день, то часы меньше текущего нельзя выбрать
+							if (day.diff(currentDay, 'days') === 0) {
+								if (hour < currentHour) {
+									return false;
+								}
+							}
 
-                this.ui.$appealCloseDate = this.$('.appeal-close-date');
-                this.ui.$appealCloseTime = this.$('.appeal-close-time');
-                this.ui.$closeDocs = this.$('.close-docs');
+							return true;
+						},
+						onMinuteShow: function (hour, minute) {
+							var day = moment(self.ui.$appealCloseDate.datepicker('getDate')).startOf('day');
+							var currentDay = moment().startOf('day');
+							var currentHour = moment().hour();
+							var currentMinute = moment().minute();
+							//если выбран текущий день и час, то минуты меньше текущего времени нельзя выбрать
+							if (day.diff(currentDay, 'days') === 0) {
+								if (hour === currentHour && minute <= currentMinute) {
+									return false;
+								}
+							}
+							return true;
+						}
+					}).timepicker('setTime', datetime);
 
-                var datetime = new Date();
+					this.results = new Results();
+					this.results
+						.fetch({data: {appealId: self.options.appeal.get('id')}})
+						.done(function () {
+							self.ui.$results = self.$('#results');
 
-                this.ui.$appealCloseDate.datepicker({
-                    minDate: 0,
-                    onSelect: function(dateText, inst) {
+							self.results.each(function (item) {
+								console.log('item', item);
+								self.ui.$results.append($('<option/>', {
+									'text': item.get('name'),
+									'value': item.get('id')
+								}));
+							}, self);
 
-                        var day = moment(self.$(this).datepicker('getDate')).startOf('day');
-                        var currentDay = moment().startOf('day');
-                        var currentHour = moment().hour();
-                        var hour = self.ui.$appealCloseTime.timepicker('getHour');
-                        //если выбрана текущая дата и время в таймпикере меньше текущего, то сбрасываем таймпикер
-                        if (day.diff(currentDay, 'days') === 0) {
-                            if (hour <= currentHour) {
-                                self.ui.$appealCloseTime.timepicker('setTime', (new Date())).trigger('change');
-                            }
-                        }
-                    }
-                }).datepicker('setDate', datetime);
+							self.ui.$results.select2();
+						});
+				}, this);
 
-                this.ui.$appealCloseTime.timepicker({
-                    showPeriodLabels: false,
-                    onHourShow: function(hour) {
-                        var day = moment(self.ui.$appealCloseDate.datepicker('getDate')).startOf('day');
-                        var currentDay = moment().startOf('day');
-                        var currentHour = moment().hour();
-                        //если выбран текущий день, то часы меньше текущего нельзя выбрать
-                        if (day.diff(currentDay, 'days') === 0) {
-                            if (hour < currentHour) {
-                                return false;
-                            }
-                        }
+				self.moves.fetch();
+			}, this);
 
-                        return true;
-                    },
-                    onMinuteShow: function(hour, minute) {
-                        var day = moment(self.ui.$appealCloseDate.datepicker('getDate')).startOf('day');
-                        var currentDay = moment().startOf('day');
-                        var currentHour = moment().hour();
-                        var currentMinute = moment().minute();
-                        //если выбран текущий день и час, то минуты меньше текущего времени нельзя выбрать
-                        if (day.diff(currentDay, 'days') === 0) {
-                            if (hour === currentHour && minute <= currentMinute) {
-                                return false;
-                            }
-                        }
-                        return true;
-                    }
-                }).timepicker('setTime', datetime);
+			this.docs4closing.fetch();
 
-
-
-                this.results = new Results();
-                this.results.fetch({
-                    data: {
-                        appealId: self.options.appeal.get('id')
-                    }
-                }).done(function() {
-                    self.ui.$results = self.$('#results');
-
-                    self.results.each(function(item) {
-                        console.log('item', item);
-                        self.ui.$results.append($('<option/>', {
-                            'text': item.get('name'),
-                            'value': item.get('id')
-                        }));
-
-                    }, self);
-
-                    self.ui.$results.select2();
-
-                });
-
-                //}
-
-
-            }, this);
-
-            this.docs4closing.fetch();
-
-
-            return this;
-        }
-
-
-    }).mixin([popupMixin]);
-
-
-
+			return this;
+		}
+	}).mixin([popupMixin]);
 });
