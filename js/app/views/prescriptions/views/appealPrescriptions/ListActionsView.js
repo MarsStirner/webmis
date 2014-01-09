a4d4ea132afae35edb74cda24a1461492da01329
Alexandr Voronkin <admin@elasticweb.ru>
2014-01-09 19:04:00 +0400

    [wip] prescription type

--- js/app/views/prescriptions/views/appealPrescriptions/ListActionsView.js
+++ js/app/views/prescriptions/views/appealPrescriptions/ListActionsView.js
@@ -1,22 +1,29 @@
-define(function(require) {
-	var template = require('text!views/prescriptions/templates/appeal/prescriptions-actions.html');
-	var BaseView = require('views/prescriptions/views/BaseView');
+define(function (require) {
+    var template = require('text!views/prescriptions/templates/appeal/prescriptions-actions.html');
+    var BaseView = require('views/prescriptions/views/BaseView');
     var PrescriptionNewView = require('views/prescriptions/views/appealPrescriptions/PrescriptionNewView');
+    var CreatePrescriptionButton = require('views/prescriptions/views/appealPrescriptions/CreatePrescriptionButton');
 
-	return BaseView.extend({
-		template: template,
-		initialize: function(){
-//create-prescription
-		},
-        data: function(){
-        	var data = {appeal:this.options.appeal.toJSON()};
-        	console.log('data', data)
+    return BaseView.extend({
+        template: template,
+        initialize: function () {
+            var createPrescriptionButtonView = new CreatePrescriptionButton({
+
+            });
+            this.addSubViews({
+                '.create-prescription-button': createPrescriptionButtonView
+            });
+
+
+        },
+        data: function () {
+            var data = {
+                appeal: this.options.appeal.toJSON()
+            };
+            console.log('data', data)
             return data;
         },
-		events: {
-			'click [data-create-prescription]':'createPrescription'
-		},
-		createPrescription: function(){
+        createPrescription: function () {
             var popup = new PrescriptionNewView({
                 actionTypeId: 754,
                 appeal: this.options.appeal,
@@ -26,13 +33,10 @@ define(function(require) {
             popup.render();
             popup.open();
 
-			// App.Router.navigate(["appeals", this.options.appeal.get('id'), "prescriptions/new"].join("/"), {trigger: true});
-
-			console.log('create prescription')
-		},
-        afterRender: function(){
-            this.$el.find('button').button();
+        },
+        afterRender: function () {
+        //    this.$el.find('button').button();
         }
-	});
+    });
 
 });
