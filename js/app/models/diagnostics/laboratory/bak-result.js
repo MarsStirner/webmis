58faaff7ddf060cfc23e0ddb9affa92cca0838c2
Alexandr Voronkin <admin@elasticweb.ru>
2014-01-27 19:14:03 +0400

    вывод антибиотикограммы

--- js/app/models/diagnostics/laboratory/bak-result.js
+++ js/app/models/diagnostics/laboratory/bak-result.js
@@ -1,9 +1,50 @@
 define(["models/model-base"], function (ModelBase) {
-	//return ModelBase.extend({
-	return Backbone.Model.extend({
-		url: function () {
-			//return DATA_PATH + "diagnostics/laboratory/bak/" + this.diagnosticId;
-			return "/bak-results.json"
-		}
-	});
+    return ModelBase.extend({
+//    return Backbone.Model.extend({
+        url: function () {
+            return DATA_PATH + "diagnostics/laboratory/bak/" + this.diagnosticId;
+            //return "/bak-results.json";
+        },
+        getTable: function () {
+            var organisms = this.get('organismResults');
+            var antibiotics = [];
+
+            _.each(organisms, function (organism) {
+                var sensValues = organism.sensValues;
+                _.each(sensValues, function (sensValue) {
+                    var antibiotic = {
+                        name: sensValue.antibiotic.name,
+                        code: sensValue.antibiotic.code,
+                        organisms: []
+                    };
+                    antibiotics.push(antibiotic);
+                });
+
+            });
+
+            antibiotics = _.uniq(antibiotics, false, function(item){
+                return item.name; 
+            });
+
+            _.each(antibiotics, function(antibiotic){
+                console.log(antibiotic);
+                _.each(organisms, function (organism) {
+
+                    var sensValues = organism.sensValues;
+                    var o = {astivity: '', id: organism.organism.id, name: organism.organism.name};
+
+                    _.each(sensValues, function (sensValue) {
+                        if(sensValue.antibiotic.name === antibiotic.name){
+                            o.activity = sensValue.activity;
+                        }
+                    });
+                    antibiotic.organisms.push(o);
+                });
+            });
+
+            console.log('antibiotics', antibiotics);
+
+            return {rows: antibiotics};
+        }
+    });
 });
