13aedc6573ef133c9b18b9f5376f156bba8bd8a4
Alexandr Voronkin <admin@elasticweb.ru>
2014-02-25 12:41:09 +0400

    rewrite build

--- gulpfile.js
+++ gulpfile.js
@@ -1,68 +1,48 @@
 var gulp = require('gulp');
-var compass = require('gulp-compass');
 var clean = require('gulp-clean');
-var zip = require('gulp-zip');
-var filter = require('gulp-filter');
-var git = require('git-rev');
-var Q = require('q');
-var moment = require('moment');
+var replace = require('gulp-replace');
+var dateFormat = require('dateformat');
+var runSequence = require('gulp-run-sequence');
+var version = require('./package.json').version;
+var buildDate = dateFormat(new Date(), 'dd.mm.yy.HH.MM');
+var buildName = 'webmis-' + version + '-' + buildDate;
+var buildDir = 'dist';
+var packageDir = buildDir+'/'+buildName;
 
 
-var gitBranchPromise = function () {
-    var deferred = Q.defer();
+var filesToMove = ['css/**/*',
+    'docs/**/*',
+    'api/**/*',
+    'font/**/*',
+    'images/**/*',
+    'js/**/*',
+    'lib/**/*',
+    'pdf/**/*',
+    '.htaccess',
+    'index.html',
+    'proxy.php'
+];
 
-    git.branch(function (branchName) {
-        deferred.resolve(branchName);
-    });
-
-    return deferred.promise;
-};
-
-
-gulp.task('sass', function () {
-    return gulp.src('./sass/*.scss')
-        .pipe(compass())
-        .pipe(filter('!**/*.scss'))
-        .pipe(gulp.dest('./css'));
-});
-
-
-gulp.task('clean-dist', function () {
-    return gulp.src('./dist/*', {
+gulp.task('clean', function () {
+    return gulp.src([buildDir + '/*'], {
         read: false
     }).pipe(clean());
 });
 
-
-gulp.task('zip-build', function () {
-    var buildDate = moment().format('DDMMYYYY');
-
-    return gitBranchPromise().then(function (branchName) {
-        var buildName = branchName + '-' + buildDate;
-        return gulp.src(['css/**/*',
-            'docs/**/*',
-            'api/**/*',
-            'font/**/*',
-            'images/**/*',
-            'js/**/*',
-            'lib/**/*',
-            'pdf/**/*',
-            '.htaccess',
-            'index.html',
-            'proxy.php'
-        ])
-            .pipe(zip(buildName + '.zip'))
-            .pipe(gulp.dest('dist'));
-    });
-
+gulp.task('buildName', function () {
+    return gulp.src(['js/common.js'],{base:'./'})
+        .pipe(replace(/var GUI_VERSION = "(\w+)"/g, 'var GUI_VERSION = "' + buildName + '"'))
+        .pipe(gulp.dest(packageDir + '/'));
+});
+gulp.task('move', function () {
+    return gulp.src(filesToMove, {
+        base: './', read: true
+    }).pipe(gulp.dest(packageDir));
 });
 
-
-gulp.task('default', function () {
-    gulp.run('sass');
-});
-
-
-gulp.task('build', function () {
-    gulp.run('sass', 'clean-dist', 'zip-build');
+gulp.task('default', function (cb) {
+    runSequence('clean', 
+                'move', 
+                'buildName',
+                cb);
 });
