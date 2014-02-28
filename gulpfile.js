6cf4f04c994521e78758fb4eed5419a92741d8aa
Alexandr Voronkin <admin@elasticweb.ru>
2014-02-28 15:50:33 +0400

    add jenkins build number support

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,23 @@ var clean = require('gulp-clean');
 var replace = require('gulp-replace');
 var dateFormat = require('dateformat');
 var runSequence = require('gulp-run-sequence');
+var args = require('optimist').argv;
+
+var buildDir = 'dist';
+
 var version = require('./package.json').version;
 var buildDate = dateFormat(new Date(), 'dd.mm.yy.HH.MM');
-var buildName = 'webmis-' + version + '-' + buildDate;
-var buildDir = 'dist';
-var packageDir = buildDir+'/'+buildName;
+var buildNumber = args.build;
+
+var buildName = [];
+buildName.push('webmis', version);
+if (buildNumber) {
+    buildName.push(buildNumber);
+}
+buildName.push(buildDate);
+buildName = buildName.join('-');
+
+var packageDir = buildDir + '/' + buildName;
 
 
 var filesToMove = ['css/**/*',
@@ -30,19 +42,22 @@ gulp.task('clean', function () {
 });
 
 gulp.task('buildName', function () {
-    return gulp.src(['js/common.js'],{base:'./'})
+    return gulp.src(['js/common.js'], {
+        base: './'
+    })
         .pipe(replace(/var GUI_VERSION = "(\w+)"/g, 'var GUI_VERSION = "' + buildName + '"'))
         .pipe(gulp.dest(packageDir + '/'));
 });
 gulp.task('move', function () {
     return gulp.src(filesToMove, {
-        base: './', read: true
+        base: './',
+        read: true
     }).pipe(gulp.dest(packageDir));
 });
 
 gulp.task('default', function (cb) {
-    runSequence('clean', 
-                'move', 
-                'buildName',
-                cb);
+    runSequence('clean',
+        'move',
+        'buildName',
+        cb);
 });
