a625799a3b2d3d3ed96c7a5c81cc4217f7637711
Alexandr Voronkin <admin@elasticweb.ru>
2014-03-19 18:09:50 +0400

    fix webmis-401

--- js/app/views/appeal/edit/pages/monitoring/views/ExecPersonAssignmentDialog.js
+++ js/app/views/appeal/edit/pages/monitoring/views/ExecPersonAssignmentDialog.js
@@ -1,186 +1,189 @@
-define(function(require) {
-	var shared = require('views/appeal/edit/pages/monitoring/shared');
+define(function (require) {
+    var shared = require('views/appeal/edit/pages/monitoring/shared');
 
-	var assignExecPersonDialogTmpl = require('text!templates/appeal/edit/pages/monitoring/assign-exec-person-dialog.tmpl');
+    var assignExecPersonDialogTmpl = require('text!templates/appeal/edit/pages/monitoring/assign-exec-person-dialog.tmpl');
 
-	var BaseView = require('views/appeal/edit/pages/monitoring/views/BaseView');
+    var BaseView = require('views/appeal/edit/pages/monitoring/views/BaseView');
 
 
 
-	/**
-	 * Диалог назначения врача
-	 * @type {*}
-	 */
-	var ExecPersonAssignmentDialog = BaseView.extend({
-		template: assignExecPersonDialogTmpl,
+    /**
+     * Диалог назначения врача
+     * @type {*}
+     */
+    var ExecPersonAssignmentDialog = BaseView.extend({
+        template: assignExecPersonDialogTmpl,
 
-		data: function() {
-			return {
-				assignMe: this.assignMe
-			};
-		},
+        data: function () {
+            return {
+                assignMe: this.assignMe
+            };
+        },
 
-		events: {
-			"change input[name='filter-persons']": "onFilterPersonsChange",
-			"click .assign-on-me": "onAssignOnMeClick"
-		},
+        events: {
+            "change input[name='filter-persons']": "onFilterPersonsChange",
+            "click .assign-on-me": "onAssignOnMeClick"
+        },
 
-		initialize: function(options) {
-			BaseView.prototype.initialize.apply(this);
-			_.bindAll(this);
-			this.appeal = options.appeal;
-			this.allPersons = options.allPersons;
-			this.departmentPersons = options.departmentPersons;
-			this.appealExecPerson = options.appealExecPerson;
-			this.collections = [];
+        initialize: function (options) {
+            BaseView.prototype.initialize.apply(this);
+            _.bindAll(this);
+            this.appeal = options.appeal;
+            this.allPersons = options.allPersons;
+            this.departmentPersons = options.departmentPersons;
+            this.appealExecPerson = options.appealExecPerson;
+            this.collections = [];
 
-			//Все врачи
-			this.allPersons.setParams({
-				limit: 999,
-				sortingField: 'lastname'
-			});
-			//this.allPersons.on("reset", this.addAllPersons, this).fetch();
+            //Все врачи
+            this.allPersons.setParams({
+                limit: 999,
+                sortingField: 'lastname'
+            });
+            //this.allPersons.on("reset", this.addAllPersons, this).fetch();
 
-			this.getCollection('allPersons');
-			// console.log('col', this.getCollection('allPersons'))
+            this.getCollection('allPersons');
+            // console.log('col', this.getCollection('allPersons'))
 
-			//Врачи отделения
-			this.departmentPersons.setParams({
-				limit: 999,
-				sortingField: 'lastname',
-				filter: {
-					departmentId: this.appeal.get("currentDepartment").id
-				}
-			});
-			this.getCollection('departmentPersons');
-			// this.departmentPersons.on("reset", this.addDepartmentPersons, this).fetch();
+            //Врачи отделения
+            this.departmentPersons.setParams({
+                limit: 999,
+                sortingField: 'lastname',
+                filter: {
+                    departmentId: this.appeal.get("currentDepartment").id
+                }
+            });
+            this.getCollection('departmentPersons');
+            // this.departmentPersons.on("reset", this.addDepartmentPersons, this).fetch();
 
-			this.assignMe = true;
+            this.assignMe = true;
 
-			if ((Core.Cookies.get("currentRole") === 'nurse-department') || (Core.Cookies.get("currentRole") === 'nurse-receptionist')) {
-				this.assignMe = false;
-			}
+            if ((Core.Cookies.get("currentRole") === 'nurse-department') || (Core.Cookies.get("currentRole") === 'nurse-receptionist')) {
+                this.assignMe = false;
+            }
 
-		},
+        },
 
-		getCollection: function(collectionName) {
-			if (!this.collections[collectionName]) {
-				this[collectionName].fetch();
-				this.collections[collectionName] = this[collectionName];
-			}
-			return this.collections[collectionName];
-		},
+        getCollection: function (collectionName) {
+            if (!this.collections[collectionName]) {
+                this[collectionName].fetch();
+                this.collections[collectionName] = this[collectionName];
+            }
+            return this.collections[collectionName];
+        },
 
-		open: function() {
-			this.$el.dialog({
-				title: "Назначение лечащего врача",
-				modal: true,
-				width: "50em",
-				dialogClass: "webmis",
-				resizable: false,
-				buttons: [{
-					text: "Назначить",
-					click: this.assignExecPerson,
-					"class": "button-color-green"
-				}, {
-					text: "Отмена",
-					click: this.close
-				}],
-				close: this.close
-			});
+        open: function () {
+            this.$el.dialog({
+                title: "Назначение лечащего врача",
+                modal: true,
+                width: "50em",
+                dialogClass: "webmis",
+                resizable: false,
+                buttons: [{
+                    text: "Назначить",
+                    click: this.assignExecPerson,
+                    "class": "button-color-green"
+                }, {
+                    text: "Отмена",
+                    click: this.close
+                }],
+                close: this.close
+            });
 
-			this.$el.css({
-				"min-height": "11em"
-			});
-		},
+            this.$el.css({
+                "min-height": "11em"
+            });
+        },
 
-		close: function() {
-			this.allPersons.off(null, null, this);
-			this.departmentPersons.off(null, null, this);
-			this.off(null, null, this).remove();
-		},
+        close: function () {
+            console.log('close')
+            this.$el.dialog("close");
+            this.allPersons.off(null, null, this);
+            this.departmentPersons.off(null, null, this);
+            // this.off(null, null, this).remove();
+            // this.remove();
+        },
 
-		onFilterPersonsChange: function(event) {
-			this.$(".all-persons,.department-persons").select2("val", "");
+        onFilterPersonsChange: function (event) {
+            this.$(".all-persons,.department-persons").select2("val", "");
 
-			var selectedFilter = this.getSelectedFilter();
+            var selectedFilter = this.getSelectedFilter();
 
-			if (selectedFilter === "all") {
-				this.$(".all-persons.select2-container").show();
-				this.$(".department-persons.select2-container").hide();
-			} else if (selectedFilter === "dep") {
-				this.$(".all-persons.select2-container").hide();
-				this.$(".department-persons.select2-container").show();
-			}
-		},
+            if (selectedFilter === "all") {
+                this.$(".all-persons.select2-container").show();
+                this.$(".department-persons.select2-container").hide();
+            } else if (selectedFilter === "dep") {
+                this.$(".all-persons.select2-container").hide();
+                this.$(".department-persons.select2-container").show();
+            }
+        },
 
-		onAssignOnMeClick: function(event) {
-			event.preventDefault();
+        onAssignOnMeClick: function (event) {
+            event.preventDefault();
 
-			var currentUserId = Core.Cookies.get("userId");
+            var currentUserId = Core.Cookies.get("userId");
 
-			this.$("input[name='filter-persons'][value='all']").prop("checked", true).change();
-			this.$(".all-persons").select2("val", currentUserId).change();
-		},
+            this.$("input[name='filter-persons'][value='all']").prop("checked", true).change();
+            this.$(".all-persons").select2("val", currentUserId).change();
+        },
 
-		getSelectedFilter: function() {
-			return this.$("input[name='filter-persons']:checked").val();
-		},
+        getSelectedFilter: function () {
+            return this.$("input[name='filter-persons']:checked").val();
+        },
 
-		assignExecPerson: function() {
-			var selectedFilter = this.getSelectedFilter();
+        assignExecPerson: function () {
+            var selectedFilter = this.getSelectedFilter();
 
-			var selectedExecPersonId;
+            var selectedExecPersonId;
 
-			if (selectedFilter === "all") {
-				selectedExecPersonId = this.$(".all-persons").select2("val");
-			} else if (selectedFilter === "dep") {
-				selectedExecPersonId = this.$(".department-persons").select2("val");
-			}
+            if (selectedFilter === "all") {
+                selectedExecPersonId = this.$(".all-persons").select2("val");
+            } else if (selectedFilter === "dep") {
+                selectedExecPersonId = this.$(".department-persons").select2("val");
+            }
 
-			if (selectedExecPersonId) {
-				var appealExecPerson = this.appealExecPerson;
-				appealExecPerson.save({
-					id: selectedExecPersonId
-				});
+            if (selectedExecPersonId) {
+                var appealExecPerson = this.appealExecPerson;
+                appealExecPerson.save({
+                    id: selectedExecPersonId
+                });
 
-				this.appeal.set("execPerson", this.allPersons.get(selectedExecPersonId).toJSON());
+                this.appeal.set("execPerson", this.allPersons.get(selectedExecPersonId).toJSON());
 
-				this.close();
-			}
-		},
+                this.close();
+            }
+        },
 
-		addAllPersons: function() {
-			var allPersons = this.getCollection('allPersons');
-			this.$(".all-persons").append(allPersons.map(function(person) {
-				return "<option value='" + person.get('id') + "'>" + person.get("name").raw + "</option>";
-			})).select2("enable");
-		},
+        addAllPersons: function () {
+            var allPersons = this.getCollection('allPersons');
+            this.$(".all-persons").append(allPersons.map(function (person) {
+                return "<option value='" + person.get('id') + "'>" + person.get("name").raw + "</option>";
+            })).select2("enable");
+        },
 
-		addDepartmentPersons: function() {
-			var departmentPersons = this.getCollection('departmentPersons');
-			this.$(".department-persons").append(departmentPersons.map(function(person) {
-				return "<option value='" + person.get('id') + "'>" + person.get("name").raw + "</option>";
-			})).select2("enable");
-		},
+        addDepartmentPersons: function () {
+            var departmentPersons = this.getCollection('departmentPersons');
+            this.$(".department-persons").append(departmentPersons.map(function (person) {
+                return "<option value='" + person.get('id') + "'>" + person.get("name").raw + "</option>";
+            })).select2("enable");
+        },
 
-		render: function() {
-			BaseView.prototype.render.apply(this);
-			this.addAllPersons();
-			this.addDepartmentPersons();
+        render: function () {
+            BaseView.prototype.render.apply(this);
+            this.addAllPersons();
+            this.addDepartmentPersons();
 
-			this.$("#filter-persons-container").buttonset();
-			this.$(".all-persons, .department-persons").select2({
-				matcher: function(term, text, opt) {
-					return text.split(' ')[0].toUpperCase().indexOf(term.toUpperCase()) >= 0
-				}
-			})//.select2("disable");
-			this.$(".all-persons").hide();
+            this.$("#filter-persons-container").buttonset();
+            this.$(".all-persons, .department-persons").select2({
+                matcher: function (term, text, opt) {
+                    return text.split(' ')[0].toUpperCase().indexOf(term.toUpperCase()) >= 0
+                }
+            }) //.select2("disable");
+            this.$(".all-persons").hide();
 
-			return this;
-		}
-	});
+            return this;
+        }
+    });
 
-	return ExecPersonAssignmentDialog;
+    return ExecPersonAssignmentDialog;
 
 });
