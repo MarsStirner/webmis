3550f2ba51ff715e9a5e8c2292b42c941affb26d
vladislove <oleynick.v@gmail.com>
2014-05-30 15:35:25 +0400

    WEBMIS-413

--- js/app/views/appeal/edit/pages/monitoring/collections/PatientDiagnoses.js
+++ js/app/views/appeal/edit/pages/monitoring/collections/PatientDiagnoses.js
@@ -128,7 +128,99 @@ define(function(require) {
 
 			}, this);
 
-			return data;
+			if (!data.length) {
+				//getting first move data
+				var Moves = require('collections/moves/moves');
+				var moves = new Moves();
+				var diagnoses = [];
+				var diagnosesAttrs = [];
+				var diagReceived = {};
+				var attendant = {};
+				var aftereffect = {};
+				moves.appealId = this.appealId;
+				$.ajaxSetup({async: false});
+				moves.fetch();
+				if (moves.models.length) {
+					$.getJSON(DATA_PATH + "appeals/" + moves.appealId + "/documents/" + moves.models[0].get('id') + "?callback=?", function (response) {
+						firstMoveDatetime = $.grep(response.data[0].group[0].attribute, function(e){ return e.name == 'assessmentBeginDate'; })[0].value;
+						var firstMoveAttrs = response.data[0].group[1].attribute;
+						var moveDate = $.grep(response.data[0].group[0].attribute, function(e){ return e.name == 'assessmentBeginDate'; })[0].properties[0].value;
+						moveDate = new Date(moveDate).valueOf();
+						//getting diagnoses from first move attributes
+						$.each(firstMoveAttrs, function(i, attr){
+							if (attr.code === 'diagReceivedMkb') {
+								var diagReceivedAttr = $.grep(attr.properties, function(e){ return e.name == 'value'; });
+								var diagReceivedId = $.grep(attr.properties, function(e){ return e.name == 'valueId'; });
+								if (diagReceivedAttr.length > 0) {
+									diagReceived.id = diagReceivedId[0].value;
+									diagReceived.diagnosis = diagReceivedAttr[0].value;
+									diagReceived.label = attr.name;
+									diagReceived.kind = 'diagReceivedMkb';
+									diagReceived.datetime = moveDate;
+									diagnosesAttrs.push(diagReceived);
+								}
+							} else if (attr.id == 1500) {
+								var attendantAttr  = $.grep(attr.properties, function(e){ return e.name == 'value'; });
+								var attendantId = $.grep(attr.properties, function(e){ return e.name == 'valueId'; });
+								if (attendantAttr.length > 0) {
+									attendant.id = attendantId[0].value;
+									attendant.diagnosis = attendantAttr[0].value;
+									attendant.label = attr.name;
+									attendant.kind = 'attendantMkb';
+									attendant.datetime = moveDate;
+									diagnosesAttrs.push(attendant);
+								}
+							} else if (attr.id == 1501) {
+								var attendantDescriptionAttr  = $.grep(attr.properties, function(e){ return e.name == 'value'; });
+								if (attendantDescriptionAttr.length > 0) {
+									attendant.description = $(attendantDescriptionAttr[0].value).text();
+								}
+							} else if (attr.id == 1506) {
+								var diagReceivedDescriptionAttr  = $.grep(attr.properties, function(e){ return e.name == 'value'; });
+								if (diagReceivedDescriptionAttr.length > 0) {
+									diagReceived.description = $(diagReceivedDescriptionAttr[0].value).text();
+								}
+							} else if (attr.id == 1507) {
+								var aftereffectAttr  = $.grep(attr.properties, function(e){ return e.name == 'value'; });
+								var aftereffectId = $.grep(attr.properties, function(e){ return e.name == 'valueId'; });
+								if (aftereffectAttr.length > 0) {
+									aftereffect.id = aftereffectId[0].value;
+									aftereffect.diagnosis = aftereffectAttr[0].value;
+									aftereffect.label = attr.name;
+									aftereffect.kind = 'aftereffectMkb';
+									aftereffect.datetime = moveDate;
+									diagnosesAttrs.push(aftereffect);
+								}
+							} else if (attr.id == 1508) {
+								var aftereffectDescriptionAttr  = $.grep(attr.properties, function(e){ return e.name == 'value'; });
+								if (aftereffectDescriptionAttr.length > 0) {
+									aftereffect.description = $(aftereffectDescriptionAttr[0].value).text();
+								}
+							}
+						});
+
+						$.each(diagnosesAttrs, function(i, item){
+							diagnoses.push({
+								datetime: item.datetime,
+								description: item.description,
+								diagnosisKind: item.kind,
+								diagnosisKindLabel: item.label,
+								doctor: {},
+								mkb: {
+									code: '',
+									diagnosis: item.diagnosis,
+									id: item.id
+								}
+							});
+						});	
+					});
+				};
+				return diagnoses;
+				$.ajaxSetup({async: true});	
+			} else {
+				console.log(data);
+				return data;
+			}
 		}
 	});
 
