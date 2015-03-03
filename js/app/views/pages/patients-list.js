d8934e24ff3d306ba0aff9d43b8be1c88d93d76f
vladislove <oleynick.v@gmail.com>
2015-03-03 19:41:38 +0400

    WEBMIS-81

--- js/app/views/pages/patients-list.js
+++ js/app/views/pages/patients-list.js
@@ -56,22 +56,24 @@ define(["collections/patients", "views/grid", "views/filter", "views/paginator"]
 
             this.$el.find(".Container").html(PatientsGrid.render().el);
 
-            // PatientsGrid.on("grid:rowClick", function (patient) {
-            //     this.trigger("patient:selected", patient);
-            // }, this);
             PatientsGrid.on('grid:rowClick', function (model, event) {
-                if (event.target.localName != 'a') {
-                    var url = '';
-                    if (App.Router.currentPage == "appointments") {
-                        window.location.href = '/appointments/' + model.get('id');
-                    } else {
-                        url = '/patients/' + model.get('id') + '/';
-                        window.open(url);
-                    }
+
+                if (App.Router.currentPage === 'appeals') {
+                    this.trigger("patient:selected", model);
                 } else {
-                    view.newSendToDepartment(model);
+                    if (event.target.localName != 'a') {
+                        var url = '';
+                        if (App.Router.currentPage == "appointments") {
+                            window.location.href = '/appointments/' + model.get('id');
+                        } else {
+                            url = '/patients/' + model.get('id') + '/';
+                            window.open(url);
+                        }
+                    } else {
+                        view.newSendToDepartment(model);
+                    }
                 }
-            });
+            }, this);
 
             // Пэйджинатор
             var Paginator = new App.Views.Paginator({
