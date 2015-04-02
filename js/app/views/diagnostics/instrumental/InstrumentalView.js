bf980905ad30b9b2f6e53334e6e5b365fdf4d80d
vladislove <oleynick.v@gmail.com>
2015-04-02 20:36:26 +0400

    WEBMIS-127

--- js/app/views/diagnostics/instrumental/InstrumentalView.js
+++ js/app/views/diagnostics/instrumental/InstrumentalView.js
@@ -154,6 +154,7 @@ define(function(require) {
 		},
 
 		render: function() {
+			console.log('ЗЫС', this);
 			this.$el.html(_.template(this.template,{closed: this.options.appeal.get('closed')}));
 			this.$("#grid").html(this.grid.el);
 			this.$("#grid-pager").html(this.paginator.render().el);
